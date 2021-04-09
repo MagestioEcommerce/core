1830dbe89e84e461d77823a781f2e17ebb23e5e4
Sergio Villar <sergio@magestio.com>
2021-04-09 12:25:17 +0200

    Update add multisku

--- app/code/Magestio/Core/Ui/DataProvider/Product/ProductDataProvider.php
+++ app/code/Magestio/Core/Ui/DataProvider/Product/ProductDataProvider.php
@@ -14,7 +14,24 @@ class ProductDataProvider extends DataProvider
     public function addFilter(Filter $filter)
     {
         if ($filter->getField() == 'category_id') {
-            $this->getCollection()->addCategoriesFilter(['in' => $filter->getValue()]);
+            $this->getCollection()->addCategoriesFilter(array('in' => $filter->getValue()));
+        } elseif ($filter->getField() == 'sku_multiple') {
+
+            // Remove % at the begin and end
+            $value = $filter->getValue();
+            $value = substr($value, 1, strlen($value) - 2);
+
+            //
+            $skus = explode(',', $value);
+            $skus = array_map('trim', $skus);
+
+            $filters = [];
+            foreach ($skus as $sku) {
+                $filters[] = ['attribute' => 'sku', 'like' => "%$sku%"];
+            }
+
+            $this->getCollection()->addFieldToFilter($filters);
+
         } elseif (isset($this->addFilterStrategies[$filter->getField()])) {
             $this->addFilterStrategies[$filter->getField()]
                 ->addFilter(
